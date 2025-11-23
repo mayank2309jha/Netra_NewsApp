967b02520d3b7ce087787f2558c5fadd64c5a905
Mayank Jha <78499893+mayank2309jha@users.noreply.github.com>
2025-11-23 22:43:56 +0530

    Update App.jsx

--- frontend-repo/netra-app/src/App.jsx
+++ frontend-repo/netra-app/src/App.jsx
@@ -1,18 +1,21 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import NewsPage from "./pages/NewsPage"
-
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NewsPage from "./pages/NewsPage";
+import Auth from "./pages/Auth";
+import Profile from "./pages/Profile";
+import Stats from "./pages/Stats";
 function App() {
-
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path='/article/:id' element={<NewsPage/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/article/:id" element={<NewsPage />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/stats" element={<Stats />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
